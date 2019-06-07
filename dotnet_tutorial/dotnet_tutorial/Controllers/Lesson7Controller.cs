8c057362fc6cc7b9c8708903f75d56485955e5e2
金小虎 <xiaohu.jin@merchant-s.com>
2019-06-08 02:02:33 +0900

    第7课：重定向

---
+++ dotnet_tutorial/dotnet_tutorial/Controllers/Lesson7Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MerchantSupport.Tutorial
+{
+	public class Lesson7Controller : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Show()
+		{
+			return Redirect("/lesson2");
+		}
+	}
+}
