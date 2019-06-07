ee3d897eeef76803ed28ad3284164e7d6ba53bd7
金小虎 <xiaohu.jin@merchant-s.com>
2019-06-08 01:36:11 +0900

    第4课：复杂的模型

---
+++ dotnet_tutorial/dotnet_tutorial/Controllers/Lesson4Controller.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MerchantSupport.Tutorial
+{
+	public class Lesson4Controller : Controller
+	{
+		public class Holiday
+		{
+			public DateTime Day { get; set; }
+			public string Name { get; set; }
+		}
+
+		public IActionResult Index()
+		{
+			var model = new Holiday[]
+			{
+				new Holiday
+				{
+					Day = new DateTime(2019,1,1),
+					Name = "元旦"
+				},
+				new Holiday
+				{
+					Day = new DateTime(2019,10,1),
+					Name = "国庆"
+				},
+				new Holiday
+				{
+					Day = new DateTime(2019, 12, 25),
+					Name = "圣诞"
+				}
+			};
+			return View(model);
+		}
+	}
+
+}
