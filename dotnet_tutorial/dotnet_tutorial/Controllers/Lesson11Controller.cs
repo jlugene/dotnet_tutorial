509cca32092d65c812a4c5e98cc3ec587c97aaef
金小虎 <xiaohu.jin@merchant-s.com>
2019-06-08 03:15:18 +0900

    第11课：表单入门

---
+++ dotnet_tutorial/dotnet_tutorial/Controllers/Lesson11Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MerchantSupport.Tutorial
+{
+	public class Lesson11Controller : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public string Show(string holidayName, string holidayDate)
+		{
+			return $"假日日期：{holidayDate}; 假日名称：{holidayName}";
+		}
+	}
+}
