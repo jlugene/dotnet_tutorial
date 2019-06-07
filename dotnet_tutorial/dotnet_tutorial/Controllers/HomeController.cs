d934a851f18a4e03b66535faf95feb973c36527c
金小虎 <xiaohu.jin@merchant-s.com>
2019-06-08 03:14:57 +0900

    fix

--- dotnet_tutorial/dotnet_tutorial/Controllers/HomeController.cs
+++ dotnet_tutorial/dotnet_tutorial/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Reflection;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace MerchantSupport.Tutorial
 {
@@ -13,8 +14,15 @@ namespace MerchantSupport.Tutorial
 				.Where(t => typeof(Controller).IsAssignableFrom(t))
 				.Where(t => t != typeof(HomeController))
 				.Select(t => t.Name.Replace("Controller", ""))
+				.OrderBy(t => GetLessonNo(t))
 				.ToArray();
 			return View(items);
+		}
+
+		private int GetLessonNo(string name)
+		{
+			var match = Regex.Match(name, @"\d+");
+			return match.Success ? int.Parse(match.Value) : 0;
 		}
 	}
 }
