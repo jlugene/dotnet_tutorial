18c54adca02a3352f769f9d9d7fc3fde9ea17118
金小虎 <xiaohu.jin@merchant-s.com>
2019-06-08 01:58:52 +0900

    第6课：Action

---
+++ dotnet_tutorial/dotnet_tutorial/Controllers/Lesson6Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MerchantSupport.Tutorial
+{
+	public class Lesson6Controller : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+
+		public IActionResult Show()
+		{
+			return View();
+		}
+
+		public IActionResult List()
+		{
+			return View();
+		}
+	}
+
+}
