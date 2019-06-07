92123f1ff173078d92acfcc5750089181cbc5fdf
金小虎 <xiaohu.jin@merchant-s.com>
2019-06-08 02:35:31 +0900

    第9课：使用模板

---
+++ dotnet_tutorial/dotnet_tutorial/Controllers/Lesson9Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MerchantSupport.Tutorial
+{
+	public class Lesson9Controller : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
