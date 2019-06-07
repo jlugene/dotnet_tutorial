daf3baf0c823c50afcd64e14c411304eab0973b2
金小虎 <xiaohu.jin@merchant-s.com>
2019-06-08 01:14:11 +0900

    第1课：控制器

---
+++ dotnet_tutorial/dotnet_tutorial/Controllers/Lesson1Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MerchantSupport.Tutorial
+{
+	public class Lesson1Controller : Controller
+	{
+		public string Index()
+		{
+			return "Hello, World!";
+		}
+	}
+}
