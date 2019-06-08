be0e02d1ccb542ded4967bf719e638595f980319
金小虎 <xiaohu.jin@merchant-s.com>
2019-06-09 03:38:25 +0900

    第24课：删除数据

---
+++ dotnet_tutorial/dotnet_tutorial/Controllers/Lesson24Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MerchantSupport.Tutorial
+{
+	//第24课：删除数据
+	public class Lesson24Controller : Lesson23Controller
+	{
+		[HttpPost]
+		public IActionResult Delete(string id)
+		{
+			T_Holiday.DeleteById(id);
+			var referer = this.Request.Headers["Referer"].ToString();
+			return Redirect(referer);
+		}
+	}
+}
