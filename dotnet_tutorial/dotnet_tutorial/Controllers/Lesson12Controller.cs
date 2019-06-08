a49d0a5cd115548d7baf1e17a2d048d59695d99f
金小虎 <xiaohu.jin@merchant-s.com>
2019-06-08 11:23:34 +0900

    第12课：模型绑定

---
+++ dotnet_tutorial/dotnet_tutorial/Controllers/Lesson12Controller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MerchantSupport.Tutorial
+{
+	public class Lesson12Controller : Controller
+	{
+		public class Holiday
+		{
+			public string Day { get; set; }
+			public string Name { get; set; }
+		}
+
+		public IActionResult Index()
+		{
+			return View(new Holiday());
+		}
+
+		public IActionResult Show(Holiday holiday)
+		{
+			return Content($"假日日期：{holiday.Day}; 假日名称：{holiday.Name}");
+		}
+	}
+}
