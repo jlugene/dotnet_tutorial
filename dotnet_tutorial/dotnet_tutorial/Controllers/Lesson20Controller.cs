d3fe0fc2cd85253ee6b66c297d8a19bbfb12a684
金小虎 <xiaohu.jin@merchant-s.com>
2019-06-09 02:09:31 +0900

    第20课 数据库查询 - 列表显示

---
+++ dotnet_tutorial/dotnet_tutorial/Controllers/Lesson20Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace MerchantSupport.Tutorial
+{
+	//第20课：数据库查询 - 单页显示列表
+	public class Lesson20Controller : Controller
+	{
+		public IActionResult Index()
+		{
+			var model = T_Holiday.FindAll().ToArray();
+			return View(model);
+		}
+	}
+}
