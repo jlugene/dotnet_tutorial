fb18cc7cbc15cf917ce31b11a8ba25defdbbf4fc
金小虎 <xiaohu.jin@merchant-s.com>
2019-06-08 14:14:26 +0900

    第17课 模型验证扩展库

---
+++ dotnet_tutorial/dotnet_tutorial/Controllers/Lesson17Controller.cs
@@ -0,0 +1,36 @@
+﻿using MerchantSupport.Lib;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace MerchantSupport.Tutorial
+{
+	public class Lesson17Controller : Controller
+	{
+		public class Holiday
+		{
+			[DisplayName("日期")]
+			[NotEmpty]
+			[Date]
+			public string Day { get; set; }
+
+			[DisplayName("名称")]
+			[NotEmpty]
+			public string Name { get; set; }
+		}
+
+		public IActionResult Index()
+		{
+			return View(new Holiday());
+		}
+
+		[HttpPost]
+		public IActionResult Show(Holiday holiday)
+		{
+			if(!ModelState.IsValid)
+			{
+				return View("Index", holiday);
+			}
+			return Content($"假日日期：{holiday.Day}; 假日名称：{holiday.Name}");
+		}
+	}
+}
