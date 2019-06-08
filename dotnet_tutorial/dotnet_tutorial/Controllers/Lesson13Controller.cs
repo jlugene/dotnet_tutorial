8a830484a5ce60bbcedf47603404580c87f68280
金小虎 <xiaohu.jin@merchant-s.com>
2019-06-08 11:24:22 +0900

    第13课：POST方法

---
+++ dotnet_tutorial/dotnet_tutorial/Controllers/Lesson13Controller.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MerchantSupport.Tutorial
+{
+	public class Lesson13Controller : Controller
+	{
+		public class Holiday
+		{
+			public string Day { get; set; }
+			public string Name { get; set; }
+		}
+
+		public IActionResult Index()
+		{
+			return View(new Holiday());
+		}
+
+		[HttpPost]
+		public IActionResult Show(Holiday holiday)
+		{
+			return Content($"假日日期：{holiday.Day}; 假日名称：{holiday.Name}");
+		}
+	}
+}
